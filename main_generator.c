f4afc0e4fa817d2a789de816a9712e43996f56a7
Vlad Petrenko <HarewVlad@mail.ru>
2019-02-17 00:12:29 +0300

    Add files via upload

---
+++ main_generator.c
@@ -0,0 +1,192 @@
+// expr3 = INT | '(' expr ')'
+// expr2 = [-]expr3
+// expr1 = expr2 ([/*]expr2)*
+// expr0 = expr1 ([+-]expr1)*
+// expr = expr0
+
+typedef enum TokenKind
+{
+	TOKEN_INT,
+	TOKEN_CHAR,
+} TokenKind;
+
+typedef struct Token
+{
+	TokenKind kind;
+	union
+	{
+		int val;
+		char ch;
+	};
+
+} Token;
+
+Token token;
+const char *stream;
+FILE *output;
+
+void next_token()
+{
+	repeat:
+	switch (*stream)
+	{
+		case ' ':
+			stream++;
+			goto repeat;
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+			int value = 0;
+			while (isdigit(*stream))
+			{
+				value *= 10;
+				value += *stream++ - '0';
+			}
+			token.val = value;
+			token.kind = TOKEN_INT;
+			break;
+		default:
+			token.kind = *stream++;
+			break;
+	}
+}
+
+inline bool is_token(TokenKind kind)
+{
+	return token.kind == kind;
+}
+
+inline bool match_token(TokenKind kind)
+{
+	if (is_token(kind))
+	{
+		next_token();
+		return true;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+inline bool expect_token(TokenKind kind)
+{
+	if (is_token(kind))
+	{
+		next_token();
+		return true;
+	}
+	else
+		fatal("Unexpected token %s", kind);
+}
+
+int parse_expr();
+
+int parse_expr3()
+{
+	if (is_token(TOKEN_INT))
+	{
+		int val = token.val;
+		write_string(output, "00000001 ; LIT\n");
+		write_digit(output, val);
+		next_token();
+		return val;
+	}
+	else if (match_token('('))
+	{
+		int val = parse_expr();
+		expect_token(')');
+		return val;
+	}
+	else
+	{
+		fatal("expected integer or (, got %s", token.kind);
+		return 0;
+	}
+}
+
+int parse_expr2()
+{
+	if (match_token('~'))
+	{
+		return -parse_expr3();
+	}
+	else
+		return parse_expr3();
+}
+
+int parse_expr1()
+{
+	int val = parse_expr2();
+	while (is_token('/') || is_token('*'))
+	{
+		char op = token.kind;
+		next_token();
+		int rval = parse_expr2();
+
+		if (op == '/')
+		{
+			write_string(output, "00000101 ; DIV\n");
+			assert(rval != 0);
+			val /= rval;
+		}
+		else if (op == '*')
+		{
+			write_string(output, "00000100 ; MUL\n");
+			val *= rval;
+		}
+	}
+	return val;
+}
+
+int parse_expr0()
+{
+	int val = parse_expr1();
+	while (is_token('+') || is_token('-'))
+	{
+		char op = token.kind;
+		next_token();
+		int rval = parse_expr1();
+		if (op == '+')
+		{
+			write_string(output, "00000010 ; ADD\n");
+			val += rval;
+		}
+		else if (op == '-')
+		{
+			write_string(output, "00000011 ; SUB\n");
+			val -= rval;
+		}
+	}
+	return val;
+}
+
+int parse_expr()
+{
+	return parse_expr0();
+}
+
+void init_stream(const char *source)
+{
+	stream = source;
+	next_token();
+}
+
+void generate_code(const char *source, const char *path)
+{
+	output = fopen(path, "r+");
+	if (!output)
+		fatal("Cant open the file");
+
+	init_stream(source);
+	parse_expr();
+	write_string(output, "00000110 ; HALT\n");
+	fclose(output);
+}
---
+++ utils.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <limits.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdarg.h>
+
+void fatal(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    printf("FATAL: ");
+    vprintf(fmt, args);
+    printf("\n");
+    va_end(args);
+    exit(1);
+}
+
+void write_string(FILE *f, const char *str)
+{
+	size_t len = strlen(str);
+	if (!fwrite(str, 1, len, f))
+		fatal("Cant write the string");
+}
+
+int msb_index(int val)
+{
+	int index = 0;
+	for (int shift = 0; val; val >>= 1, shift++)
+	{	
+		if (val & 1)
+			index = shift;
+	}
+	return index;
+}
+
+void write_digit(FILE *f, int val) // TODO: sign
+{
+	int index = msb_index(val);
+	int padding = 31 - index - 1;
+
+	char str[32]; // 1 - '\0'
+	memset(str, '0', 32);
+
+	for (int i = 31, temp_val = val; i > padding; i--, temp_val >>= 1)
+	{
+		str[i] = (temp_val & 1) + '0';
+	}
+
+	char comment[3 + 10 + 1 + 1]; // 3 - " ; ", 10 - "xxxxxxxxxx", 1 - '\n', 1 - '\0'
+	sprintf(comment, " ; %d\n", val);
+
+	if (!fwrite(str, 1, 32, f))
+		fatal("Cant write the string");
+	if (!fwrite(comment, 1, strlen(comment), f))
+		fatal("Cant write the string");
+}
